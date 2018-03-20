d2a0ab6be00189f419026ea1d50ebdc375ca39d1
szimano <szimano@szimano.org>
2018-03-20 15:44:35 -0400

    reformatting

--- circuit-breaker/hystrix-backend/src/main/scala/com/softwaremill/sttp/hystrix/HystrixBackend.scala
+++ circuit-breaker/hystrix-backend/src/main/scala/com/softwaremill/sttp/hystrix/HystrixBackend.scala
@@ -5,14 +5,16 @@ import com.netflix.hystrix.{HystrixCommand, HystrixCommandGroupKey, HystrixComma
 import com.softwaremill.sttp.{MonadAsyncError, MonadError, Request, Response, SttpBackend}
 import rx.{Observable, Subscriber}
 
-class HystrixBackend[R[_], S] private(delegate: SttpBackend[R, S])(groupKey: String, setter: HystrixCommandProperties.Setter)
-  extends SttpBackend[R, S] {
+class HystrixBackend[R[_], S] private (delegate: SttpBackend[R, S])(groupKey: String,
+                                                                    setter: HystrixCommandProperties.Setter)
+    extends SttpBackend[R, S] {
 
   class AsyncSttpCMD[T](request: Request[T, S], delegate: SttpBackend[R, S])
-    extends HystrixObservableCommand[Response[T]](
-      Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(groupKey))
-        .andCommandPropertiesDefaults(setter)
-    ) {
+      extends HystrixObservableCommand[Response[T]](
+        Setter
+          .withGroupKey(HystrixCommandGroupKey.Factory.asKey(groupKey))
+          .andCommandPropertiesDefaults(setter)
+      ) {
 
     override def construct(): Observable[Response[T]] = Observable.create(
       (t: Subscriber[_ >: Response[T]]) => {
@@ -21,38 +23,41 @@ class HystrixBackend[R[_], S] private(delegate: SttpBackend[R, S])(groupKey: Str
           t.onCompleted()
           responseMonad.unit(response)
         }
-        responseMonad.handleError(x){ case e: Throwable =>
-          t.onError(e)
-          responseMonad.error(e)
+        responseMonad.handleError(x) {
+          case e: Throwable =>
+            t.onError(e)
+            responseMonad.error(e)
         }
       }
     )
   }
 
   class SyncSttpCMD[T](request: Request[T, S], delegate: SttpBackend[R, S])
-    extends HystrixCommand[R[Response[T]]](
-      HystrixCommand.Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(groupKey))
-        .andCommandPropertiesDefaults(setter)
-    ) {
+      extends HystrixCommand[R[Response[T]]](
+        HystrixCommand.Setter
+          .withGroupKey(HystrixCommandGroupKey.Factory.asKey(groupKey))
+          .andCommandPropertiesDefaults(setter)
+      ) {
     override def run(): R[Response[T]] = delegate.send(request)
   }
 
   override def send[T](request: Request[T, S]): R[Response[T]] = {
 
     responseMonad match {
-      case mae: MonadAsyncError[R] => mae.async { cb =>
+      case mae: MonadAsyncError[R] =>
+        mae.async { cb =>
+          new AsyncSttpCMD(request, delegate)
+            .observe()
+            .subscribe(new Subscriber[Response[T]]() {
+              def onNext(item: Response[T]): Unit = {
+                cb(Right(item))
+              }
 
-        new AsyncSttpCMD(request, delegate).observe().subscribe(
-          new Subscriber[Response[T]]() {
-            def onNext(item: Response[T]): Unit = {
-              cb(Right(item))
-            }
+              def onError(error: Throwable): Unit = cb(Left(error))
 
-            def onError(error: Throwable): Unit = cb(Left(error))
-
-            def onCompleted(): Unit = {}
-          })
-      }
+              def onCompleted(): Unit = {}
+            })
+        }
 
       case _ => new SyncSttpCMD(request, delegate).execute()
     }
@@ -68,7 +73,8 @@ class HystrixBackend[R[_], S] private(delegate: SttpBackend[R, S])(groupKey: Str
 }
 
 object HystrixBackend {
-  def apply[R[_], S](delegate: SttpBackend[R, S])
-                    (groupKey: String, setter: HystrixCommandProperties.Setter = HystrixCommandProperties.Setter()) =
+  def apply[R[_], S](delegate: SttpBackend[R, S])(groupKey: String,
+                                                  setter: HystrixCommandProperties.Setter =
+                                                    HystrixCommandProperties.Setter()) =
     new HystrixBackend(delegate)(groupKey, setter)
 }
